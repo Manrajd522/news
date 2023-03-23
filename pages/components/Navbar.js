8f3e8e3b7cc6ed43397da11dc4d013a717aee8fb
Manraj singh <83108581+Manrajd522@users.noreply.github.com>
2023-03-23 23:20:17 +0530

    Update Navbar.js
    add linkedin

--- pages/components/Navbar.js
+++ pages/components/Navbar.js
@@ -8,6 +8,8 @@ const Navbar = () => {
         <div className={styles.main}>
             <div onClick={() => router.push('/')}>Home</div>
             <div onClick={() => router.push('/feed/1')}>Feed</div>
+            <div onClick={() => (window.location.href = 'https://www.linkedin.com/in/manrajd522/')}>Linkedin</div>
+
             
              
         </div>
